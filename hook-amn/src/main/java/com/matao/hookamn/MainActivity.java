1a639aed889c3ddd3f2dd5e6c823eb08e5b8d1dc
matao <muxter.matao@gmail.com>
2019-01-20 15:43:55 +0800

    refactor: adjust hook entrance

--- hook-amn/src/main/java/com/matao/hookamn/MainActivity.java
+++ hook-amn/src/main/java/com/matao/hookamn/MainActivity.java
@@ -1,5 +1,6 @@
 package com.matao.hookamn;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,11 +8,15 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    protected void attachBaseContext(Context newBase) {
+        HookHelper.hookActivityManager();
+        super.attachBaseContext(newBase);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        HookHelper.hookActivityManager();
 
         Intent intent = new Intent(this, SecondActivity.class);
         startActivity(intent);
